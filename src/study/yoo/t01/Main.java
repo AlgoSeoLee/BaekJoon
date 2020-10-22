761cc04569167375ca10fac237671f597f064b0e
Seo, Yujin <tjdbwls8998@naver.com>
2020-10-22 14:06:55 +0900

    test

--- src/study/yoo/t01/Main.java
+++ src/study/yoo/t01/Main.java
@@ -2,6 +2,6 @@ package study.yoo.t01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world hi");
     }
 }
