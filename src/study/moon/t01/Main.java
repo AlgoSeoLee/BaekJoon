078f3f4bf549b1e18795ad04881b1ef3674a66d8
MoonHKLee <mf311110@gmail.com>
2020-10-22 13:58:57 +0900

    first init

---
+++ src/study/moon/t01/Main.java
@@ -0,0 +1,4 @@
+package study.moon.t01;
+
+public class Main {
+}
---
+++ src/study/yoo/t01/Main.java
@@ -0,0 +1,7 @@
+package study.yoo.t01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
