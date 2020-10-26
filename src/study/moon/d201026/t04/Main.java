cf3d2dea36030f0769a0fb0198424ec62ac37f2a
MoonHKLee <mf311110@gmail.com>
2020-10-26 22:04:07 +0900

    1022 algo update

---
+++ src/study/moon/d201026/t04/Main.java
@@ -0,0 +1,25 @@
+package study.moon.d201026.t04;
+
+import java.io.*;
+
+// https://www.acmicpc.net/problem/14890
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] split = br.readLine().split(" ");
+        int size = Integer.parseInt(split[0]);
+        int n = Integer.parseInt(split[1]);
+        int[][] arr = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            int floor = arr[i][0];
+            for (int j = 0; j < size; j++) {
+                if (floor == arr[i][j]) {
+                    continue;
+                }
+
+            }
+        }
+    }
+}
