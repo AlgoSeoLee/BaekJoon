80eea484dbd3d5452b0030e9dc73248ba3b24893
Seo, Yujin <tjdbwls8998@naver.com>
2020-11-19 21:01:27 +0900

    201119 commit

---
+++ src/study/yoo/d201119/t03/Main.java
@@ -0,0 +1,47 @@
+package study.yoo.d201119.t03;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] truck_weights = {7,4,5,6};
+		int[] truck_weights1 = {10};
+		int[] truck_weights2 = {10,10,10,10,10,10,10,10,10,10};
+		System.out.println(solution(2, 10, truck_weights));
+		System.out.println(solution(100, 100, truck_weights1));
+		System.out.println(solution(100, 100, truck_weights2));
+	}
+
+	
+	
+	    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+	        int[] endTime = new int[truck_weights.length]; // 트럭이 다리를 모두 지나는 시간
+	        
+	        Queue<Integer> onBridge = new LinkedList<Integer>();
+	        int time = 0 , cur=0; //현재 무게
+	        while(true) {
+	            // 도착한 버스 제거 
+	            if(!onBridge.isEmpty() && endTime[onBridge.peek()] == time) {
+	                weight += truck_weights[onBridge.poll()];
+	            }
+	            
+	            // 대기하는 버스 추가 
+	            if(cur < truck_weights.length && truck_weights[cur] <= weight) {
+	                onBridge.add(cur);
+	                endTime[cur] = time + bridge_length; // 다리에 올라가는 시간 + 다리 길이
+	                weight -= truck_weights[cur];
+	                cur++;
+	            }
+	            
+	            time++;
+	            if(onBridge.isEmpty())
+	                break;
+	        }
+	        return time;
+	    }
+
+	
+}
