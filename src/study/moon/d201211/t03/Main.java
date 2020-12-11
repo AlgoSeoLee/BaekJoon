559e1ef472c9d5d971d90cf36e0591b330f5ec7a
MoonHKLee <mf311110@gmail.com>
2020-12-11 18:40:58 +0900

    1211 algorithm update

---
+++ src/study/moon/d201211/t03/Main.java
@@ -0,0 +1,83 @@
+package study.moon.d201211.t03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+//https://www.acmicpc.net/problem/2630
+public class Main {
+
+    static int a = 0;
+    static int b = 0;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(s[j]);
+            }
+        }
+        int papers = getPapers(0, 0, n);
+        if (papers != -1) {
+            if (papers == 0) {
+                a++;
+            } else {
+                b++;
+            }
+        }
+        bw.write(a + "\n");
+        bw.write(b + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    private static int getPapers(int startX, int startY, int size) {
+        size /= 2;
+        if (size == 0) {
+            return arr[startX][startY];
+        }
+        int[] papers = new int[4];
+        int count = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                papers[count] = getPapers(startX + i * size, startY + j * size, size);
+                count++;
+            }
+        }
+        if (isAllSame(papers) != -1) {
+            return isAllSame(papers);
+        } else {
+            addPapers(papers);
+            return -1;
+        }
+    }
+
+    private static int isAllSame(int[] papers) {
+        int value = papers[0];
+        for (int i = 1; i < 4; i++) {
+            if (value != papers[i]) {
+                return -1;
+            }
+        }
+        return value;
+    }
+
+    private static void addPapers(int[] papers) {
+        for (int i = 0; i < 4; i++) {
+            if (papers[i] == 0) {
+                a++;
+            }
+            if (papers[i] == 1) {
+                b++;
+            }
+        }
+    }
+
+}
--- src/study/moon/test/Main.java
+++ src/study/moon/test/Main.java
@@ -1,16 +1,35 @@
 package study.moon.test;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 //https://www.acmicpc.net/problem/10814
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String[] arr = {"128","My", "Name","IS","MunHyeok"};
+        ArrayDeque<Integer> list = new ArrayDeque<>();
+        String[] arr ={"1","1","2","3","5","8"};
+        arr = reverse(arr);
+        arr = reverse(arr);
+        arr = delete(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    private static String[] delete(String[] split) {
+        String[] arr = new String[split.length - 1];
+        if (arr.length >= 0) {
+            System.arraycopy(split, 1, arr, 0, arr.length);
+        }
+        return arr;
+    }
 
-        boolean matches = "128".matches("^[0-9]*$");
-        System.out.println(matches);
+    private static String[] reverse(String[] split) {
+        String[] arr = new String[split.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = split[split.length - 1 - i];
+        }
+        return arr;
     }
 }
 
