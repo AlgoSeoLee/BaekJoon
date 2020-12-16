b741bc90deeddd137a141c5f9d9d08463c97ad49
MoonHKLee <mf311110@gmail.com>
2020-12-16 16:33:12 +0900

    1216 algorithm update

---
+++ src/study/moon/d201216/t02/Main.java
@@ -0,0 +1,36 @@
+package study.moon.d201216.t02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < T; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (maxHeap.isEmpty()) {
+                    bw.write("0\n");
+                }else {
+                    bw.write(maxHeap.poll()+"\n");
+                }
+            } else {
+                maxHeap.add(x);
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
