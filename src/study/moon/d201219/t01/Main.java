fba5da2049a01f8a2990830cce27738c7b0a619e
MoonHKLee <mf311110@gmail.com>
2020-12-19 14:08:42 +0900

    1219 algorithm update

---
+++ src/study/moon/d201219/t01/Main.java
@@ -0,0 +1,33 @@
+package study.moon.d201219.t01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        Map<String,String> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String[] s1 = br.readLine().split(" ");
+            map.put(s1[0],s1[1]);
+        }
+        for (int i = 0; i < m; i++) {
+            String s1 = br.readLine();
+            bw.write(map.get(s1)+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
