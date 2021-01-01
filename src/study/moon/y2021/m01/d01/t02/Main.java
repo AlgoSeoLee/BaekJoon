7ec7dbc3f52c1a13ce200de022b7a936f66a79b4
MoonHKLee <mf311110@gmail.com>
2021-01-01 22:38:40 +0900

    0101 algorithm update

--- src/study/moon/y2021/m01/d01/t02/Main.java
+++ src/study/moon/y2021/m01/d01/t02/Main.java
@@ -14,13 +14,14 @@ public class Main {
     static ArrayList<ArrayList<Node>> list; // 인접리스트.
 
     static int cost;
-    static List<Integer> path = new ArrayList<>();
+    static int[] parent;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         vertex = Integer.parseInt(br.readLine());
         edge = Integer.parseInt(br.readLine());
+        parent = new int[vertex+1];
         list = new ArrayList<>();
 
         for (int i = 0; i <= vertex; i++) {
@@ -40,9 +41,22 @@ public class Main {
 
         String[] split = br.readLine().split(" ");
 
-        dijkstra(Integer.parseInt(split[0]),Integer.parseInt(split[1]));
+        int start = Integer.parseInt(split[0]);
+        int end = Integer.parseInt(split[1]);
+        dijkstra(start,end);
+        List<Integer> answer = new ArrayList<>();
+        while (parent[end]!=0) {
+            answer.add(0,end);
+            end = parent[end];
+        }
+        answer.add(0,start);
 
-        System.out.println(path);
+        bw.write(cost +"\n");
+        bw.write(answer.size()+"\n");
+        for (Integer integer : answer) {
+            bw.write(integer + " ");
+        }
+
         bw.flush();
         bw.close();
         br.close();
@@ -57,8 +71,6 @@ public class Main {
 
         heap.offer(new Node(start, 0));
         dist[start] = 0;
-        path.add(start);
-        path.add(end);
 
         while (!heap.isEmpty()) {
             Node curNode = heap.poll();
@@ -70,9 +82,7 @@ public class Main {
                     if (!check[node.index] && dist[node.index] > dist[index] + node.data) {
                         dist[node.index] = dist[index] + node.data;
                         heap.add(new Node(node.index, dist[node.index]));
-                        if (!path.contains(index)) {
-                            path.add(path.size()-1,index);
-                        }
+                        parent[node.index] = index;
                     }
                 }
             }
