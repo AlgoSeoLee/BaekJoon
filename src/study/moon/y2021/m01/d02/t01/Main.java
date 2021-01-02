93bb9d26230f53296a76d88733e206c8782a28c6
MoonHKLee <mf311110@gmail.com>
2021-01-02 22:08:58 +0900

    0102 algorithm update

---
+++ src/study/moon/y2021/m01/d02/t01/Main.java
@@ -0,0 +1,66 @@
+package study.moon.y2021.m01.d02.t01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Main {
+
+    static int[] visit;
+    static LinkedList<Data> queue;
+    static int answer = 0;
+    static int endTime = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int k = Integer.parseInt(split[1]);
+        visit = new int[100001];
+        Arrays.fill(visit,-1);
+        queue = new LinkedList<>();
+        queue.offer(new Data(n, 0));
+
+        while (!queue.isEmpty()) {
+            Data poll = queue.poll();
+            if (poll.time > endTime) {
+                continue;
+            }
+            if (poll.position < 0 || poll.position > 100000) {
+                continue;
+            }
+            if (visit[poll.position]!=-1) {
+                if (poll.time != visit[poll.position]){
+                    continue;
+                }
+            }
+            if (poll.position == k) {
+                answer++;
+                endTime = poll.time;
+            }
+            visit[poll.position] = poll.time;
+            queue.offer(new Data(poll.position - 1, poll.time + 1));
+            queue.offer(new Data(poll.position + 1, poll.time + 1));
+            queue.offer(new Data(poll.position * 2, poll.time + 1));
+        }
+        System.out.println(endTime);
+        System.out.println(answer);
+    }
+
+}
+
+class Data {
+
+    int position;
+    int time;
+
+    public Data(int position, int time) {
+        this.position = position;
+        this.time = time;
+    }
+}
---
+++ src/study/moon/y2021/m01/d02/t02/Main.java
@@ -0,0 +1,60 @@
+package study.moon.y2021.m01.d02.t02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static boolean[] visit;
+    static PriorityQueue<Data> queue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int k = Integer.parseInt(split[1]);
+        visit = new boolean[100001];
+        queue = new PriorityQueue<>();
+        queue.offer(new Data(n, 0));
+
+        while (!queue.isEmpty()) {
+            Data poll = queue.poll();
+            if (poll.position < 0 || poll.position > 100000) {
+                continue;
+            }
+            if (visit[poll.position]) {
+                continue;
+            }
+            if (poll.position==k) {
+                System.out.println(poll.time);
+                break;
+            }
+            visit[poll.position] = true;
+            queue.offer(new Data(poll.position * 2, poll.time));
+            queue.offer(new Data(poll.position - 1, poll.time + 1));
+            queue.offer(new Data(poll.position + 1, poll.time + 1));
+        }
+    }
+
+}
+
+class Data implements Comparable<Data> {
+
+    int position;
+    int time;
+
+    public Data(int position, int time) {
+        this.position = position;
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(Data o) {
+        return this.time-o.time;
+    }
+}
