7e94e6006b1414558cc7b0acce217d2b7a4f734e
MoonHKLee <mf311110@gmail.com>
2021-01-03 19:11:43 +0900

    0103 algorithm update

---
+++ src/study/moon/y2021/m01/d03/t01/Main.java
@@ -0,0 +1,62 @@
+package study.moon.y2021.m01.d03.t01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        int n = Integer.parseInt(s[0]);
+        int r = Integer.parseInt(s[1]);
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        int[] output = new int[n];
+
+        permutation(arr, output, 0, n, r);
+        bw.flush();
+        bw.close();
+    }
+
+
+    //순열
+    //사용 예시: permutation(arr, output, visited, 0, n, 3, answer);
+    static void permutation(int[] arr, int[] output, int depth, int n, int r) throws IOException {
+        if (depth == r) {
+            permPrint(output, r);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            output[depth] = arr[i];
+            permutation(arr, output, depth + 1, n, r);
+        }
+    }
+
+    //순열 출력
+    static void permPrint(int[] output, int r) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < r; i++) {
+            if (i == 0) {
+                builder.append(output[i]).append(" ");
+            }else {
+                if (output[i]< output[i-1]) {
+                    return;
+                } else {
+                    builder.append(output[i]).append(" ");
+                }
+            }
+        }
+        bw.write(builder.toString()+"\n");
+    }
+}
---
+++ src/study/moon/y2021/m01/d03/t02/Main.java
@@ -0,0 +1,60 @@
+package study.moon.y2021.m01.d03.t02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        int n =Integer.parseInt(s[0]);
+        int r =Integer.parseInt(s[1]);
+        s = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(s[i]);
+        }
+        Arrays.sort(arr);
+        int[] output = new int[n];
+        boolean[] visited = new boolean[n];
+
+
+        permutation(arr, output, visited, 0, n, r);
+        bw.flush();
+        bw.close();
+
+    }
+
+    //순열
+    //사용 예시: permutation(arr, output, visited, 0, n, 3, answer);
+    static void permutation(int[] arr, int[] output, boolean[] visited, int depth, int n, int r)
+        throws IOException {
+        if (depth == r) {
+            permPrint(output, r);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                permutation(arr, output, visited, depth + 1, n, r);
+                visited[i] = false;
+            }
+        }
+    }
+
+    //순열 출력
+    static void permPrint(int[] arr, int r)throws IOException {
+        for (int i = 0; i < r; i++) {
+            bw.write(arr[i]+" ");
+        }
+        bw.write("\n");
+    }
+}
