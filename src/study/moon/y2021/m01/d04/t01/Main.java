92f945dcaa4bd59b17b9f6b06199590cd5d89ba6
MoonHKLee <mf311110@gmail.com>
2021-01-04 11:31:33 +0900

    0104 algorithm update

---
+++ src/study/moon/y2021/m01/d04/t01/Main.java
@@ -0,0 +1,127 @@
+package study.moon.y2021.m01.d04.t01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+
+public class Main {
+
+    static LinkedList<Place> queue = new LinkedList<>();
+    static int n;
+    static int m;
+    static int[] moveX = {-1, 1, 0, 0};
+    static int[] moveY = {0, 0, -1, 1};
+    static int[][] arr;
+    static int COUNT;
+    static int ANSWER = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] s = br.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
+
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            s = br.readLine().split(" ");
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(s[j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 2) {
+                    queue.offer(new Place(i, j));
+                }
+                if (arr[i][j] == 0) {
+                    COUNT++;
+                }
+            }
+        }
+        COUNT-=3;
+
+        //3개 뽑기
+        combination(0, getCopy());
+
+        bw.write(ANSWER + "");
+        bw.flush();
+        bw.close();
+    }
+
+    static void combination(int depth, int[][] copy) {
+        if (depth == 3) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (copy[i][j] == 2) {
+                        queue.offer(new Place(i, j));
+                    }
+                }
+            }
+            bfs(copy);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (copy[i][j] == 0) {
+                    copy[i][j] = 1;
+                    combination(depth + 1, copy);
+                    copy[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    static int[][] getCopy() {
+        int[][] copy = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            if (m >= 0) {
+                System.arraycopy(arr[i], 0, copy[i], 0, m);
+            }
+        }
+        return copy;
+    }
+
+    private static void bfs(int[][] copy) {
+        int count = COUNT;
+        boolean[][] visit = new boolean[n][m];
+        while (!queue.isEmpty()) {
+            Place poll = queue.poll();
+            if (poll.n < 0 || poll.n >= n || poll.m < 0 || poll.m >= m) {
+                continue;
+            }
+
+            if (copy[poll.n][poll.m] == 1 || visit[poll.n][poll.m]) {
+                continue;
+            }
+            visit[poll.n][poll.m] = true;
+            if (copy[poll.n][poll.m]==0) {
+                count--;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                queue.add(new Place(poll.n + moveX[i], poll.m + moveY[i]));
+            }
+        }
+        if (count > ANSWER) {
+            ANSWER = count;
+        }
+    }
+
+}
+
+class Place {
+
+    int n;
+    int m;
+
+    public Place(int n, int m) {
+        this.n = n;
+        this.m = m;
+    }
+}
---
+++ src/study/moon/y2021/m01/d04/t02/Main.java
@@ -0,0 +1,113 @@
+package study.moon.y2021.m01.d04.t02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static int vertex, edge, range;
+    static ArrayList<ArrayList<Node>> list; // 인접리스트.
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] split = br.readLine().split(" ");
+        vertex = Integer.parseInt(split[0]);
+        range = Integer.parseInt(split[1]);
+        edge = Integer.parseInt(split[2]);
+
+        split = br.readLine().split(" ");
+        int[] item = new int[vertex + 1];
+        for (int i = 1; i <= vertex; i++) {
+            item[i] = Integer.parseInt(split[i - 1]);
+        }
+
+        list = new ArrayList<>();
+
+        for (int i = 0; i <= vertex; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        //양방향 인접리스트 구현
+        for (int i = 0; i < edge; i++) {
+            String[] strings = br.readLine().split(" ");
+            int start = Integer.parseInt(strings[0]);
+            int end = Integer.parseInt(strings[1]);
+            int weight = Integer.parseInt(strings[2]);
+
+            // start에서 end로 가는 weight (가중치)
+            list.get(start).add(new Node(end, weight));
+            list.get(end).add(new Node(start, weight));
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= vertex; i++) {
+            int[] result = dijkstra(i);
+
+            int count = 0;
+            for (int j = 1; j <= vertex; j++) {
+                if (result[j] <= range) {
+                    count+=item[j];
+                }
+            }
+            if (count > answer) {
+                answer = count;
+            }
+        }
+
+        bw.write(answer + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    // 다익스트라 알고리즘
+    public static int[] dijkstra(int start) {
+        PriorityQueue<Node> heap = new PriorityQueue<>();
+        boolean[] check = new boolean[vertex + 1];
+        int[] dist = new int[vertex + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        heap.offer(new Node(start, 0));
+        dist[start] = 0;
+
+        while (!heap.isEmpty()) {
+            Node curNode = heap.poll();
+            int index = curNode.index;
+
+            if (!check[index]) {
+                check[index] = true;
+                for (Node node : list.get(index)) {
+                    if (!check[node.index] && dist[node.index] > dist[index] + node.data) {
+                        dist[node.index] = dist[index] + node.data;
+                        heap.add(new Node(node.index, dist[node.index]));
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+}
+
+class Node implements Comparable<Node> {
+
+    int index;
+    int data;
+
+    Node(int index, int data) {
+        this.index = index;
+        this.data = data;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return data - o.data;
+    }
+
+}
