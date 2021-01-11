54547a7dd06c946e92fe1a0bc30eb8b0a519d529
foreunwoo <agasiya99@gmail.com>
2021-01-11 13:13:54 +0900

    210111 Swea

---
+++ src/study/eunwoo/SWEA/S1945.java
@@ -0,0 +1,32 @@
+package study.eunwoo.SWEA;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class S1945 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = { 2, 3, 5, 7, 11 };
+
+        int T = sc.nextInt();
+
+        for (int test=1; test<=T; test++) {
+            int N = sc.nextInt();
+
+            int [] pow = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                while (N % arr[i] == 0) {
+                    pow[i]++;
+                    N /= arr[i];
+                }
+            }
+
+            System.out.print("#"+test+" ");
+            for(int p: pow) {
+                System.out.print(p + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/study/eunwoo/SWEA/S1946.java
+++ src/study/eunwoo/SWEA/S1946.java
@@ -1,23 +1,41 @@
 package study.eunwoo.SWEA;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 // [1946 간단한 압축 풀기] https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PmkDKAOMDFAUq&categoryId=AV5PmkDKAOMDFAUq&categoryType=CODE
 public class S1946 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
         for (int test=1; test<=T; test++) {
-            int N = sc.nextInt();
+            int N = Integer.parseInt(br.readLine());
 
-            String alpha;
-            int count;
+            char [] alpha = new char[N];
+            int [] count = new int[N];
 
             for (int i=0; i<N; i++) {
-                alpha = sc.next();
-                count = sc.nextInt();
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                alpha[i] = st.nextToken().charAt(0);
+                count[i] = Integer.parseInt(st.nextToken());
             }
+
+            int cnt = 0;
+            System.out.println("#"+test);
+            for (int i=0; i<N; i++) {
+                for (int j=0; j<count[i]; j++) {
+                    System.out.print(alpha[i]);
+                    cnt++;
+
+                    if (cnt == 10) {
+                        System.out.println();
+                        cnt = 0;
+                    }
+                }
+            }
+            System.out.println();
         }
     }
 }
