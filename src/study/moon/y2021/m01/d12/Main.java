2ebb84b0edac95c531eb11ad5c1c5b79dc12fd0f
MoonHKLee <mf311110@gmail.com>
2021-01-12 15:58:12 +0900

    0112 algorithm update

--- src/study/moon/y2021/m01/d12/Main.java
+++ src/study/moon/y2021/m01/d12/Main.java
@@ -22,13 +22,14 @@ public class Main {
         dp[1] = 0;
         for (int i = 2; i <= N; i++) {
             dp[i] = dp[i - 1] + 1;
-            if (i%3==0) {
-                dp[i] = Math.min(dp[i],dp[i/3]+1);
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
-            if (i%2==0) {
-                dp[i] = Math.min(dp[i],dp[i/2]+1);
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
         }
+        bw.write(dp[N] + "\n");
         print(N);
 
         bw.flush();
@@ -36,13 +37,13 @@ public class Main {
     }
 
     private static void print(int N) throws IOException {
-        bw.write(N+" ");
-        if (N-1>=0 && dp[N-1] == dp[N]-1) {
-            print(N-1);
-        }else if (N%3==0 && dp[N/3]==dp[N]-1) {
-            print(N/3);
-        } else if (N%2==0 && dp[N/2]==dp[N]-1) {
-            print(N/2);
+        bw.write(N + " ");
+        if (N - 1 >= 0 && dp[N - 1] == dp[N] - 1) {
+            print(N - 1);
+        } else if (N % 3 == 0 && dp[N / 3] == dp[N] - 1) {
+            print(N / 3);
+        } else if (N % 2 == 0 && dp[N / 2] == dp[N] - 1) {
+            print(N / 2);
         }
     }
 }
