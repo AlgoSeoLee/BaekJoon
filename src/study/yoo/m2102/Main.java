c8c76f995ddc7e49bb774adbae86b0df935fcd7d
seoyujin0731 <45352177+seoyujin0731@users.noreply.github.com>
2021-02-18 16:33:21 +0900

    20210218 commit

---
+++ src/study/yoo/m2102/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+//정수삼각형 3
+class Solution {
+
+    public int solution(int[][] triangle) {
+
+        for(int i = triangle.length - 2; i >= 0; i--){
+            for(int j = 0; j < triangle[i].length; j++){
+                triangle[i][j] = triangle[i+1][j] > triangle[i+1][j+1] ? triangle[i][j] + triangle[i+1][j] : triangle[i][j]+ triangle[i+1][j+1];
+            }
+        }
+        return triangle[0][0];
+    }
+}
