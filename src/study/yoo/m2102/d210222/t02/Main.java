7474d5e0f9f35135e83cfd3e6463f991c8f72407
Seo, Yujin <tjdbwls8998@naver.com>
2021-02-21 23:09:41 +0900

    20210221 commit

---
+++ src/study/yoo/m2102/d210222/t02/Main.java
@@ -0,0 +1,32 @@
+package study.yoo.m2102.d210222.t02;
+
+import java.util.Scanner;
+
+// 동전0
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+
+		int[] coin = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			coin[i] = sc.nextInt();
+		}
+
+		int count = 0;
+
+		for (int i = N - 1; i >= 0; i--) {
+
+			if (coin[i] <= K) {
+				count += (K / coin[i]);
+				K = K % coin[i];
+			}
+		}
+		System.out.println(count);
+	}
+}
