a30b3b30e559e91f25748cda9eaae5647df4c0c4
MoonHKLee <mf311110@gmail.com>
2021-02-28 20:36:50 +0900

    0228 algorithm update

--- src/study/moon/y2021/m02/d25/Main.java
+++ src/study/moon/y2021/m02/d25/Main.java
@@ -8,76 +8,57 @@ import java.io.OutputStreamWriter;
 
 //https://www.acmicpc.net/problem/1034    [G5]    램프
 public class Main {
-    static int[][] arr;
-    static int n;
-    static int m;
-    static int k;
-
-    static int answer = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] split = br.readLine().split(" ");
-        n = Integer.parseInt(split[0]);
-        m = Integer.parseInt(split[1]);
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
 
-        arr = new int[n][m];
+        int[][] arr = new int[n][m];
+        String[] origin = new String[n];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            origin[i] = s;
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Character.getNumericValue(s.charAt(j));
+            }
+        }
 
-        k = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        boolean[] check = new boolean[n];
+        int max = 0;
 
-        dfs(0);
-        bw.write(answer+"");
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 0) {
+                    cnt++;
+                }
+            }
+            if ((cnt % 2 == k % 2) && cnt <= k) {
+                check[i] = true;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (check[i]) {
+                int cnt = 0;
+                for (int j = 0; j < n; j++) {
+                    if (origin[i].equals(origin[j])) {
+                        cnt++;
+                    }
+                }
+                if (max < cnt) {
+                    max = cnt;
+                }
+            }
+        }
+        bw.write(max + "");
         bw.flush();
         bw.close();
         br.close();
     }
-
-    private static void dfs(int count) {
-        if (count == k) {
-            int result = getOnColumn();
-            if (result>answer) {
-                answer = result;
-            }
-            return;
-        }
-
-        for (int i = 0; i < m; i++) {
-            reverseRow(i);
-            dfs(count+1);
-            reverseRow(i);
-        }
-    }
-
-    private static void reverseRow(int row) {
-        for (int i = 0; i < n; i++) {
-            arr[i][row] = 1 - arr[i][row];
-        }
-
-        for (int j = 0; j < n; j++) {
-            for (int l = 0; l < m; l++) {
-                System.out.print(arr[j][l]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    private static int getOnColumn() {
-        int result = 0;
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j]==1) {
-                    count++;
-                }
-            }
-            if (count == m) {
-                result++;
-            }
-        }
-        return result;
-    }
-
 }
