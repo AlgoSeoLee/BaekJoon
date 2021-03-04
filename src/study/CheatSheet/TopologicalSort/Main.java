4e0224ff5bfb4e060b4ed0138ad8c3758085c9f5
MoonHKLee <mf311110@gmail.com>
2021-03-04 16:55:38 +0900

    0304 algorithm update

--- src/study/CheatSheet/TopologicalSort/Main.java
+++ src/study/CheatSheet/TopologicalSort/Main.java
@@ -2,10 +2,13 @@ package study.CheatSheet.TopologicalSort;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Main {
 
@@ -72,3 +75,73 @@ public class Main {
 
     }
 }
+
+//문자열
+//public class Main {
+//
+//    static int vertex; //정점 개수
+//    static int edge; //간선 개수
+//    static Map<String, ArrayList<String>> adj; //각 정점이 향하고 있는 정점들을 가진 리스트
+//
+//    static LinkedList<String> queue = new LinkedList<>(); //작업 수행용 큐
+//
+//    static ArrayList<String> sortedList = new ArrayList<>(); //정렬 결과용 큐
+//
+//    static Map<String, Integer> targetedNodeCount;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        String[] arr = br.readLine().split(" ");
+//        vertex = arr.length;
+//        edge = Integer.parseInt(br.readLine());
+//        adj = new HashMap<>();
+//        targetedNodeCount = new HashMap<>();
+//        for (int i = 0; i < vertex; i++) {
+//            adj.put(arr[i], new ArrayList<>());
+//        }
+//        for (int i = 0; i < vertex; i++) {
+//            targetedNodeCount.put(arr[i], 0);
+//        }
+//
+//        //인접행렬, 방향성리스트 추가
+//        for (int i = 0; i < edge; i++) {
+//            String[] s = br.readLine().split(" ");
+//            String start = s[0];
+//            String end = s[1];
+//            adj.get(start).add(end);
+//            targetedNodeCount.put(end, targetedNodeCount.get(end) + 1);
+//        }
+//
+//        //최상위 정점들을 큐에 추가한다.
+//        for (int i = 0; i < vertex; i++) {
+//            if (targetedNodeCount.getOrDefault(arr[i], 0) == 0) {
+//                queue.add(arr[i]);
+//            }
+//        }
+//
+//        //로직 시작
+//        while (!queue.isEmpty()) {
+//            String currentNode = queue.poll();
+//            sortedList.add(currentNode);
+//            for (int i = 0; i < adj.get(currentNode).size(); i++) {
+//                String getArrowNode = adj.get(currentNode).get(i);
+//                targetedNodeCount.put(getArrowNode, targetedNodeCount.get(getArrowNode) - 1);
+//                if (targetedNodeCount.get(getArrowNode) == 0) {
+//                    queue.add(getArrowNode);
+//                }
+//            }
+//        }
+//
+//        //큐에 넣은것들이 다 넣어지고 나면 여지껏 들어온 정점들을 순서대로 출력하고 종료
+//        for (int i = 0; i < sortedList.size(); i++) {
+//            bw.write(sortedList.get(i) + "");
+//            if (i != sortedList.size()) {
+//                bw.write(" ");
+//            }
+//        }
+//        bw.flush();
+//        bw.close();
+//
+//    }
+//}
---
+++ src/study/moon/y2021/m03/d04/Main.java
@@ -0,0 +1,86 @@
+package study.moon.y2021.m03.d04;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/*
+소마코테    소마스토리앤파이터
+h g f w r
+4
+h g
+h f
+g r
+g w
+*/
+
+public class Main {
+
+    static int vertex; //정점 개수
+    static int edge; //간선 개수
+    static Map<String, LinkedList<String>> adj; //각 정점이 향하고 있는 정점들을 가진 리스트
+    static Map<String, Integer> targetedNode;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] arr = br.readLine().split(" ");
+        vertex = arr.length;
+        edge = Integer.parseInt(br.readLine());
+
+        adj = new HashMap<>();
+        for (int i = 0; i < vertex; i++) {
+            adj.put(arr[i], new LinkedList<>());
+        }
+
+        targetedNode = new HashMap<>();
+        for (int i = 0; i < vertex; i++) {
+            targetedNode.put(arr[i], 0);
+        }
+
+        //인접행렬, 방향성리스트 추가
+        for (int i = 0; i < edge; i++) {
+            String[] s = br.readLine().split(" ");
+            String start = s[0];
+            String end = s[1];
+            adj.get(start).add(end);
+            targetedNode.put(end, targetedNode.get(end) + 1);
+        }
+
+        //dfs 시작
+        for (int i = 0; i < vertex; i++) {
+            if (targetedNode.get(arr[i]) == 0) {
+                List<String> answer = new ArrayList<>();
+                answer.add(arr[i]);
+                dfs(arr[i], answer);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static void dfs(String skill, List<String> answer) {
+        if (adj.get(skill).size() == 0) {
+            for (String s : answer) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < adj.get(skill).size(); i++) {
+            String nowSkill = adj.get(skill).get(i);
+            answer.add(nowSkill);
+            dfs(nowSkill,answer);
+            answer.remove(answer.size()-1);
+        }
+    }
+}
