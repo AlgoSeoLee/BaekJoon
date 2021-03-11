40bfe1a4ce79093df54f3fa4a0609848640117d6
Crayon <lsh328328@naver.com>
2021-03-11 14:40:52 +0900

    Baekjoon1010

--- src/study/seungheon/Baekjoon/Baekjoon1010.java
+++ src/study/seungheon/Baekjoon/Baekjoon1010.java
@@ -3,25 +3,33 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Baekjoon1010 {
-	
-	public static int combination(int n, int r) {
-		if (n == r || r == 0) {
-			return 1;
-		} else {
-			return combination(n-1, r-1) + combination(n-1, r);
-		}
-	}
 
 	public static void main(String[] args) {
+		
 		Scanner scn = new Scanner(System.in);
+		int testCase =scn.nextInt();
 		
-		int testCase = scn.nextInt();
-		
-		for (int i = 0; i < testCase; i++) {
-			int N = scn.nextInt();
+		for(int i = 0; i < testCase; i++) {
+			int N= scn.nextInt();
 			int M = scn.nextInt();
 			
-			System.out.println(combination(M, N));
+			int dp[][] = new int[N+1][M+1];
+			
+			for(int n = 2; n <= N; n++)
+				dp[n][1]=0;
+			
+			for(int m = 1;m <= M; m++)
+				dp[1][m]=m;
+			
+			
+			for(int x =2 ;x <= N; x++) {
+				for(int y =2 ; y <= M; y++) {
+					dp[x][y]=dp[x][y-1]+dp[x-1][y-1];
+				}
+			}
+			
+			System.out.println(dp[N][M]);
 		}
+		
 	}
 }
